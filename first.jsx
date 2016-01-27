1ff5667add0479d76e3af5cacfe9097d6707349d
Guruprasad Kulkarni <guru@linux.com>
2016-01-27 14:59:54 +0530

    added jsx

--- first.jsx
+++ first.jsx
@@ -1,4 +1,9 @@
-/** @jsx React.DOM */
 var FirstComponent = React.createClass({
-
+	render:function(){
+		return <div>
+			<h1>{this.props.now}</h1>
+		</div>;
+	}
 });
+
+React.renderComponent(<FirstComponent now={new Date()}></FirstComponent>,document.body);
